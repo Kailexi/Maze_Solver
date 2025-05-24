a15965cf3df9e123b65687dc4343fe21c684f2ca
Kailexi <soniccally.sonic1010@gmail.com>
2025-05-24 20:13:01 +0300

    Initial Startup

---
+++ Source/Maze_Solver/Maze_Solver.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Maze_Solver.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Maze_Solver, "Maze_Solver" );
